521f381071454b1fb6fb4e3f9054e7eda8ef6200
Abdulrazzak-71 <devabdulrazzak31@gmail.com>
2026-04-28 20:55:55 +0530

    push

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Moon, Sun } from "lucide-react";
+import Counter from "./components/Counter/Counter";
+// import TodoIndex from "./components/TodoApplication/TodoIndex";
+
+function App() {
+  const [themeDark, setThemeDark] = useState(false)
+  const HandlesetTheme = () => {
+    setThemeDark(!themeDark)
+  }
+  return (
+    <div className={`${themeDark ? "bg-slate-900 text-white" : "bg-white text-slate-900"} p-3 h-screen w-screen space-y-5 font-serif duration-500`}>
+      <div className="absolute">
+        <button
+          onClick={HandlesetTheme}
+          className={`w-20 h-8 flex items-center rounded-full p-2 transition-all duration-300 ${themeDark ? "bg-gray-700" : "bg-yellow-400"}`}
+        >
+          {
+            themeDark ?
+              <h1 className="absolute">Light</h1>
+              :
+              <h1 className="absolute left-9">Dark</h1>
+          }
+          <div
+            className={`w-6 h-6 bg-white rounded-full shadow-md transform transition-all duration-300 flex items-center justify-center text-black ${themeDark ? "translate-x-11" : "-translate-x-1"}`}
+          >
+            {themeDark ? <Moon size={14} /> : <Sun size={14} />}
+          </div>
+        </button>
+      </div>
+      <div className=" relative top-10">
+        <Counter themeDark={themeDark} />
+        {/* <TodoIndex themeDark={themeDark} /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,96 @@
+
+import { useEffect, useRef, useState } from "react";
+
+const Counter = ({ themeDark }) => {
+    const [Count, setCount] = useState(0)
+    const [running, setRunning] = useState(false);
+    const timerRef = useRef(null);
+    const [speed, setSpeed] = useState(600);
+    const HandleCounterINC = () => {
+        setCount(Count + 1)
+    }
+    const HandleCounterDEC = () => {
+        if (Count <= 0) {
+            setCount(0)
+        } else {
+            setCount(Count - 1)
+        }
+    }
+    const HandleCounterREC = () => {
+        setCount(0)
+    }
+    const startInterval = () => {
+        timerRef.current = setInterval(() => {
+            setCount((c) => c + 1);
+        }, speed);
+    };
+    const HandleCounterAUTO = () => {
+        if (running) return;
+        setRunning(true);
+        timerRef.current = setInterval(() => {
+            setCount((c) => c + 1);
+        }, 600);
+
+    }
+    const HandleStop = () => {
+        clearInterval(timerRef.current);
+        timerRef.current = null;
+        setRunning(false);
+    }
+    useEffect(() => {
+        if (running) {
+            clearInterval(timerRef.current);
+            startInterval();
+        }
+    }, [speed]);
+
+    useEffect(() => {
+        if (running) {
+            startInterval();
+        }
+        return () => clearInterval(timerRef.current);
+    }, [running]);
+
+    return (
+        <div className="space-y-5 text-white">
+            <div className={`text-2xl ${themeDark ? "text-white" : "text-[#220901]"}`}>
+                <h1>*React Counter</h1>
+            </div>
+            <div className="grid place-content-center grid-flow-col gap-5">
+                {
+                    running &&
+                    <div className="relative">
+                        <button onClick={HandleStop} className={`bg-orange-500 absolute -left-10 ${themeDark ? "border-white " : "border-orange-800 "} rounded-3xl p-2 border-2`}>Stop</button>
+                    </div>
+                }
+                <span className={`text-3xl place-content-center ${themeDark ? "text-[#e9edc9]" : "text-black"}`}>Count : </span><h3 className={`text-7xl  ${themeDark ? "" : "text-[#2ec4b6]"}`}>{Count}</h3>
+            </div>
+            <div className="grid md:place-content-center md:grid-cols-4 gap-5 p-3 ">
+                <button disabled={running} onClick={HandleCounterREC} className={`${running ? "bg-blue-400 hover:cursor-not-allowed" : "bg-blue-700"}   ${themeDark ? "border-white " : "border-blue-800 "} rounded-3xl p-2 border-2`}>Reset</button>
+                <button disabled={running} onClick={HandleCounterDEC} className={`${running ? "bg-red-400 hover:cursor-not-allowed" : "bg-red-700"}  ${themeDark ? "border-white " : "border-red-800 "} rounded-3xl p-2 border-2`}>Decrement</button>
+                <button disabled={running} onClick={HandleCounterINC} className={`${running ? "bg-green-400 hover:cursor-not-allowed" : "bg-green-700"}  ${themeDark ? "border-white " : "border-green-800"} rounded-3xl p-2 border-2`}>Incriment</button>
+                <button disabled={running} onClick={HandleCounterAUTO} className={`${running ? "bg-purple-400 hover:cursor-not-allowed" : "bg-purple-700"}  ${themeDark ? "border-white " : "border-purple-800 "} rounded-3xl p-2 border-2`}>Auto Incriment</button>
+                {
+                    running ?
+                        <div className="col-span-4">
+                            <input
+                                type="range"
+                                min="100"
+                                max="2000"
+                                step="100"
+                                value={speed}
+                                onChange={(e) => setSpeed(Number(e.target.value))}
+                                className="w-full"
+                            />
+                            <p className={`text-sm ${themeDark ? "text-white" : "text-green-600"}`}>
+                                Speed: {speed} ms
+                            </p>
+                        </div>
+                        : ""
+                }
+            </div>
+        </div >
+    )
+}
+
+export default Counter;
